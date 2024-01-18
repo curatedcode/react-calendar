00f6e61cd5f5a8a17a92760e752d99f8c6ae30e9
Zackary Fotheringham <zackaryfotheringham@hotmail.com>
2024-01-18 08:11:21 -0500

    enable build minification in prod

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ["./src/index.ts"],
-	minify: false,
-	format: ["esm", "cjs"],
-	outDir: "dist",
-	target: "es2016",
-	dts: true,
-	clean: true,
-	sourcemap: true,
+  entry: ["./src/index.ts"],
+  minify: process.env.NODE_ENV === "development",
+  format: ["esm", "cjs"],
+  outDir: "dist",
+  target: "es2016",
+  dts: true,
+  clean: true,
+  sourcemap: true,
 });
